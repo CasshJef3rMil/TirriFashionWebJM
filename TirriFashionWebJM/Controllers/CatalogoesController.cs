9108146bd14898625350c706db9ed530203da892
Jeferson Josue Muñoz Lopez <Jefer10Mil@DESKTOP-EC207QH>
2024-04-21 08:10:00 -0600

    Creacion Metodo Crear catalogos

---
+++ TirriFashionWebJM/Controllers/CatalogoesController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TirriFashionWebJM.Models;
+
+namespace TirriFashionWebJM.Controllers
+{
+    public class CatalogoesController : Controller
+    {
+        private readonly TirriFashionWebJMContext _context;
+
+        public CatalogoesController(TirriFashionWebJMContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Catalogoes
+        public async Task<IActionResult> Index()
+        {
+            var tirriFashionWebJMContext = _context.Catalogos.Include(c => c.IdCategoriaNavigation).Include(c => c.IdUsuarioNavigation);
+            return View(await tirriFashionWebJMContext.ToListAsync());
+        }
+
+        // GET: Catalogoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Catalogos == null)
+            {
+                return NotFound();
+            }
+
+            var catalogo = await _context.Catalogos
+                .Include(c => c.IdCategoriaNavigation)
+                .Include(c => c.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (catalogo == null)
+            {
+                return NotFound();
+            }
+
+            return View(catalogo);
+        }
+
+        // GET: Catalogoes/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "Id", "Nombre");
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Catalogoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion,AñoFabricacion,IdUsuario,IdCategoria")] Catalogo catalogo, IFormFile imagen)
+        {
+            if (imagen != null && imagen.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await imagen.CopyToAsync(memoryStream);
+                    catalogo.Imagen = memoryStream.ToArray();
+                }
+            }
+
+
+
+            _context.Add(catalogo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+            //if (ModelState.IsValid)
+            //{
+
+            //}
+            //ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "Nombre", producto.CategoriaId);
+            //return View(producto);
+        }
+
+        // GET: Catalogoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Catalogos == null)
+            {
+                return NotFound();
+            }
+
+            var catalogo = await _context.Catalogos.FindAsync(id);
+            if (catalogo == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "Id", "Nombre", catalogo.IdCategoria);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Nombre", catalogo.IdUsuario);
+            return View(catalogo);
+        }
+
+        // POST: Catalogoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Imagen,Descripcion,AñoFabricacion,IdUsuario,IdCategoria")] Catalogo catalogo)
+        {
+            if (id != catalogo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(catalogo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CatalogoExists(catalogo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "Id", "Id", catalogo.IdCategoria);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Id", catalogo.IdUsuario);
+            return View(catalogo);
+        }
+
+        // GET: Catalogoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Catalogos == null)
+            {
+                return NotFound();
+            }
+
+            var catalogo = await _context.Catalogos
+                .Include(c => c.IdCategoriaNavigation)
+                .Include(c => c.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (catalogo == null)
+            {
+                return NotFound();
+            }
+
+            return View(catalogo);
+        }
+
+        // POST: Catalogoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Catalogos == null)
+            {
+                return Problem("Entity set 'TirriFashionWebJMContext.Catalogos'  is null.");
+            }
+            var catalogo = await _context.Catalogos.FindAsync(id);
+            if (catalogo != null)
+            {
+                _context.Catalogos.Remove(catalogo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CatalogoExists(int id)
+        {
+          return (_context.Catalogos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TirriFashionWebJM/Controllers/ReseñaController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TirriFashionWebJM.Models;
+
+namespace TirriFashionWebJM.Controllers
+{
+    public class ReseñaController : Controller
+    {
+        private readonly TirriFashionWebJMContext _context;
+
+        public ReseñaController(TirriFashionWebJMContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reseña
+        public async Task<IActionResult> Index()
+        {
+            var tirriFashionWebJMContext = _context.Reseñas.Include(r => r.IdCatalogoNavigation).Include(r => r.IdUsuarioNavigation);
+            return View(await tirriFashionWebJMContext.ToListAsync());
+        }
+
+        // GET: Reseña/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Reseñas == null)
+            {
+                return NotFound();
+            }
+
+            var reseña = await _context.Reseñas
+                .Include(r => r.IdCatalogoNavigation)
+                .Include(r => r.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reseña == null)
+            {
+                return NotFound();
+            }
+
+            return View(reseña);
+        }
+
+        // GET: Reseña/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCatalogo"] = new SelectList(_context.Catalogos, "Id", "Id");
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Id");
+            return View();
+        }
+
+        // POST: Reseña/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdCatalogo,IdUsuario,Comentario,Calificacion")] Reseña reseña)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reseña);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCatalogo"] = new SelectList(_context.Catalogos, "Id", "Id", reseña.IdCatalogo);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Id", reseña.IdUsuario);
+            return View(reseña);
+        }
+
+        // GET: Reseña/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Reseñas == null)
+            {
+                return NotFound();
+            }
+
+            var reseña = await _context.Reseñas.FindAsync(id);
+            if (reseña == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCatalogo"] = new SelectList(_context.Catalogos, "Id", "Id", reseña.IdCatalogo);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Id", reseña.IdUsuario);
+            return View(reseña);
+        }
+
+        // POST: Reseña/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdCatalogo,IdUsuario,Comentario,Calificacion")] Reseña reseña)
+        {
+            if (id != reseña.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reseña);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReseñaExists(reseña.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCatalogo"] = new SelectList(_context.Catalogos, "Id", "Id", reseña.IdCatalogo);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "Id", "Id", reseña.IdUsuario);
+            return View(reseña);
+        }
+
+        // GET: Reseña/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Reseñas == null)
+            {
+                return NotFound();
+            }
+
+            var reseña = await _context.Reseñas
+                .Include(r => r.IdCatalogoNavigation)
+                .Include(r => r.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reseña == null)
+            {
+                return NotFound();
+            }
+
+            return View(reseña);
+        }
+
+        // POST: Reseña/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Reseñas == null)
+            {
+                return Problem("Entity set 'TirriFashionWebJMContext.Reseñas'  is null.");
+            }
+            var reseña = await _context.Reseñas.FindAsync(id);
+            if (reseña != null)
+            {
+                _context.Reseñas.Remove(reseña);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReseñaExists(int id)
+        {
+          return (_context.Reseñas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
